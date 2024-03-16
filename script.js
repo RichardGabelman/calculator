907e2a860fe8d19da9c42ceac71b4a53169aef2a
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-03-16 04:24:46 -0700

    Implement basic calculator functions

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+  return a * b;
+};
+
+const divide = function(a, b) {
+  return a / b;
+};
+
+console.log(add(1, 3));
+
+console.log(subtract(4, 3));
+
+console.log(multiply(5, 20));
+
+console.log(divide(34, 10));
