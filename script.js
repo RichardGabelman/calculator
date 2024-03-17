12cc32b42b474082eeaca33805d6b3bfa3ea528a
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-03-16 17:45:41 -0700

    Implement basic numpad functionality

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ const divide = function(a, b) {
   return a / b;
 };
 
+let calc = {
+  leftOperand: '0',
+  operator: '',
+  operatorIndex: 0,
+  rightOperand: '',
+};
+
+const display = document.querySelector(".text");
 let leftNumber;
 let operator;
 let rightNumber;
@@ -34,3 +42,37 @@ const operate = function(leftNum, operator, rightNum) {
       break;
   }
 };
+
+const numpad = document.querySelectorAll(".numpad");
+numpad.forEach(item => {
+  item.addEventListener('click', e => {
+    const num = item.textContent;
+    if (display.textContent === '0') {
+      display.textContent = num;
+    } else if (display.textContent.length < 9) {
+      display.textContent = display.textContent + num;
+    }
+
+    if (calc.operator === '') {
+      calc.leftOperand = display.textContent;
+    } else {
+      calc.rightOperand = display.textContent.substring(calc.operatorIndex);
+    }
+  })
+});
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener('click', e => {
+  // Still on left operand
+  if (calc.operator === '') {
+    if (!calc.leftOperand.includes('.')) {
+      display.textContent = display.textContent + ".";
+      calc.leftOperand = display.textContent;
+    }
+  } else {
+    if (!calc.rightOperand.includes('.')) {
+      display.textContent = display.textContent + ".";
+      calc.rightOperand = display.textContent;
+    }
+  }
+})
